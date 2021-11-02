4f5a9ff6f90bf83a213cf33ffda636f7abc27980
taruahonen <2005548@laurea.fi>
2021-11-02 09:32:46 +0200

    commit on 'master'

--- src/app/pages/tabs/tabs-routing.module.ts
+++ src/app/pages/tabs/tabs-routing.module.ts
@@ -6,7 +6,26 @@ import { TabsPage } from './tabs.page';
 const routes: Routes = [
   {
     path: '',
-    component: TabsPage
+    component: TabsPage,
+    children: [
+      {
+        path: 'mail',
+        loadChildren: () => import('../mail/mail.module').then(m => m.MailPageModule)
+      },
+      {
+        path: 'mail/:id',
+        loadChildren: () => import('../details/details.module').then( m => m.DetailsPageModule)
+      },
+      {
+        path: 'meet',
+        loadChildren: () => import('../meet/meet.module').then(m => m.MeetPageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'mail',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
 
